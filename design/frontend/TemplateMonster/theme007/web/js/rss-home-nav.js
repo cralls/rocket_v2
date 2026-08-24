bcc2dd9596ceb6ba4c3e2800f3765ec2ac3fdc9b
Casey Ralls <cralls@vectorns.com>
2026-08-23 19:59:08 -0600

    Rev3A Design - add secondary tab interaction

--- design/frontend/TemplateMonster/theme007/web/js/rss-home-nav.js
+++ design/frontend/TemplateMonster/theme007/web/js/rss-home-nav.js
@@ -36,6 +36,35 @@ define(['jquery'], function ($) {
             });
         }
 
+        function activateSecondaryTab($tab, focusTab) {
+            var $secondary = $tab.closest('[data-rss-secondary-tabs]');
+            var $tabs = $secondary.find('.rss-home-nav__tab');
+            var panelId = $tab.attr('aria-controls');
+
+            $tabs.each(function () {
+                var $currentTab = $(this);
+                var currentPanelId = $currentTab.attr('aria-controls');
+
+                $currentTab
+                    .removeClass('_active')
+                    .attr('aria-selected', 'false')
+                    .attr('tabindex', '-1');
+
+                $('#' + currentPanelId).prop('hidden', true);
+            });
+
+            $tab
+                .addClass('_active')
+                .attr('aria-selected', 'true')
+                .attr('tabindex', '0');
+
+            $('#' + panelId).prop('hidden', false);
+
+            if (focusTab) {
+                $tab.trigger('focus');
+            }
+        }
+
         $toggles.on('click', function () {
             var $toggle = $(this);
 
@@ -44,6 +73,32 @@ define(['jquery'], function ($) {
             } else {
                 openPanel($toggle);
             }
+        });
+
+        $nav.on('click', '.rss-home-nav__tab', function () {
+            activateSecondaryTab($(this), false);
+        });
+
+        $nav.on('keydown', '.rss-home-nav__tab', function (event) {
+            var $tab = $(this);
+            var $tabs = $tab.closest('[data-rss-secondary-tabs]').find('.rss-home-nav__tab');
+            var index = $tabs.index($tab);
+            var nextIndex;
+
+            if (event.key === 'ArrowRight') {
+                nextIndex = (index + 1) % $tabs.length;
+            } else if (event.key === 'ArrowLeft') {
+                nextIndex = (index - 1 + $tabs.length) % $tabs.length;
+            } else if (event.key === 'Home') {
+                nextIndex = 0;
+            } else if (event.key === 'End') {
+                nextIndex = $tabs.length - 1;
+            } else {
+                return;
+            }
+
+            event.preventDefault();
+            activateSecondaryTab($tabs.eq(nextIndex), true);
         });
     };
 });
